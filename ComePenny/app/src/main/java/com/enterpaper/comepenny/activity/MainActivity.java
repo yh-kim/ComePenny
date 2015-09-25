205d5c63d1f23529fec789d4e13f195cc6ebcb79
yh-kim <btc_yh@naver.com>
2015-09-25 19:42:09 +0900

    base main_logo

---
+++ ComePenny/app/src/main/java/com/enterpaper/comepenny/activity/MainActivity.java
@@ -0,0 +1,57 @@
+package com.enterpaper.comepenny.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.enterpaper.comepenny.R;
+
+
+public class MainActivity extends ActionBarActivity {
+    Toolbar mToolBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Toolbar 생성
+        initToolbar();
+
+    }
+
+    private void initToolbar(){
+        //액션바 객체 생성
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        //액션바 설정
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
+        //액션바 숨김
+        actionBar.hide();
+
+        //툴바 설정
+        mToolBar = (Toolbar) findViewById(R.id.main_toolbar);
+        mToolBar.setContentInsetsAbsolute(0, 0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
